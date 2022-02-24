c0c0ecd76ecd21cec88c87e13a85f650554b23ec
Ayatarekk <99080000+Ayatarekk@users.noreply.github.com>
2022-02-24 20:40:44 +0200

    second

--- assets/js/ibda1.js
+++ assets/js/ibda1.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
     rtl:true,
     loop:true,
     margin:10,
+    dots:false,
     responsiveClass:true,
     responsive:{
         0:{
@@ -21,47 +22,91 @@ $(document).ready(function(){
     }
 });
 
-$num = $('.ui-card').length;
+
+
+
+$num = $('.my-card').length;
+
 $even = $num / 2;
+
 $odd = ($num + 1) / 2;
+
  
+
 if ($num % 2 == 0) {
-  $('.ui-card:nth-child(' + $even + ')').addClass('active');
-  $('.ui-card:nth-child(' + $even + ')').prev().addClass('prev');
-  $('.ui-card:nth-child(' + $even + ')').next().addClass('next');
-} else {
-  $('.ui-card:nth-child(' + $odd + ')').addClass('active');
-  $('.ui-card:nth-child(' + $odd + ')').prev().addClass('prev');
-  $('.ui-card:nth-child(' + $odd + ')').next().addClass('next');
+
+  $('.my-card:nth-child(' + $even +')').addClass('active');
+
+  $('.my-card:nth-child(' + $even +')').prev().addClass('prev');
+
+  $('.my-card:nth-child(' + $even +')').next().addClass('next');
+
+}else {
+
+  $('.my-card:nth-child(' + $odd +')').addClass('active');
+
+  $('.my-card:nth-child(' + $odd +')').prev().addClass('prev');
+
+  $('.my-card:nth-child(' + $odd +')').next().addClass('next');
+
 }
+
  
-$('.ui-card').click(function() {
+
+$('.my-card').click(function() {
+
   $slide = $('.active').width();
+
   console.log($('.active').position().left);
- 
+
+   
+
   if ($(this).hasClass('next')) {
-    $('.container').stop(false, true).animate({left: '-=' + $slide});
-  } else if ($(this).hasClass('prev')) {
-    $('.container').stop(false, true).animate({left: '+=' + $slide});
+
+    $('.card-carousel').stop(false,true).animate({left:'-=' + $slide});
+
+  }else if ($(this).hasClass('prev')) {
+
+    $('.card-carousel').stop(false,true).animate({left:'+=' + $slide});
+
   }
- 
+
+   
+
   $(this).removeClass('prev next');
+
   $(this).siblings().removeClass('prev active next');
- 
+
+   
+
   $(this).addClass('active');
+
   $(this).prev().addClass('prev');
+
   $(this).next().addClass('next');
+
 });
+
  
+
  
+
 // Keyboard nav
+
 $('html body').keydown(function(e) {
-  if (e.keyCode == 37) { // left
+
+  if (e.keyCode == 37) {// left
+
     $('.active').prev().trigger('click');
+
   }
-  else if (e.keyCode == 39) { // right
+
+  else if (e.keyCode == 39) {// right
+
     $('.active').next().trigger('click');
+
   }
+
 });
 
 
